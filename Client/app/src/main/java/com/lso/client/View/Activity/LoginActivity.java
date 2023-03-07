4cbcfff4de4427b591fb8fdf31f9266c6211fbfa
gianmadd <gi.addati@studenti.unina.it>
2023-03-07 17:35:04 +0100

    fix graficifix colori + listener login->signup

--- Client/app/src/main/java/com/lso/client/View/Activity/LoginActivity.java
+++ Client/app/src/main/java/com/lso/client/View/Activity/LoginActivity.java
@@ -2,17 +2,44 @@ package com.lso.client.View.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.lso.client.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText usernameText;
+    private EditText passwordText;
+    private Button accediFaceidButton;
+    private Button accediButton;
+    private TextView registratiLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameText = findViewById(R.id.username_text_login);
+        passwordText = findViewById(R.id.password_text_login);
+        accediFaceidButton = findViewById(R.id.accedi_faceid_button_login);
+        accediButton = findViewById(R.id.accedi_button_login);
+        registratiLink = findViewById(R.id.registrati_link_text_login);
+
+
+        registratiLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 
     @Override
