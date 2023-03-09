78015d3af46f23b5cc5efbeb5f883eb5d8699e19
gianmadd <gi.addati@studenti.unina.it>
2023-03-09 15:20:44 +0100

    aggiunte activity scelta acquisto cocktail/frullato

--- Client/app/src/main/java/com/lso/client/View/Activity/AcquistaActivity.java
+++ Client/app/src/main/java/com/lso/client/View/Activity/AcquistaActivity.java
@@ -2,6 +2,7 @@ package com.lso.client.View.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -37,14 +38,16 @@ public class AcquistaActivity extends AppCompatActivity {
         cocktailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(getApplicationContext(), AcquistaSceltaCocktailActivity.class);
+                startActivity(intent);
             }
         });
 
         frullatiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(getApplicationContext(), AcquistaSceltaFrullatiActivity.class);
+                startActivity(intent);
             }
         });
 
---
+++ Client/app/src/main/java/com/lso/client/View/Activity/AcquistaSceltaFrullatiActivity.java
@@ -0,0 +1,57 @@
+package com.lso.client.View.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.card.MaterialCardView;
+import com.lso.client.R;
+
+public class AcquistaSceltaFrullatiActivity extends AppCompatActivity {
+
+    private ImageButton backButton;
+    private MaterialCardView disponibiliButton;
+    private MaterialCardView consigliatiButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acquista_scelta_frullati);
+
+        backButton = findViewById(R.id.back_button_acquista_scelta_frullati);
+        disponibiliButton = findViewById(R.id.acquista_scelta_frullati_button_disponibili);
+        consigliatiButton = findViewById(R.id.acquista_scelta_frullati_button_consigliati);
+
+        disponibiliButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        consigliatiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        //This is used to hide/show 'Status Bar' & 'System Bar'. Swip bar to get it as visible.
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+}
