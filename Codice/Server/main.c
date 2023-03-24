d946942e8603194300864140a44bc8750d80057b
gianmadd <gi.addati@studenti.unina.it>
2023-03-24 12:27:45 +0100

    modifiche per prova metodi conversione

--- Codice/Server/main.c
+++ Codice/Server/main.c
@@ -12,45 +12,41 @@ int main(int argc, char* argv[]){
 
 
     // Inizio prova creazione oggetti e serializzazione in JSON
-
-    Bar* bar = creaBar("Bar");
+    Bar* bar = creaBar("barLSO");
     barToJson(bar);
     printf("\n");
 
-    Bevanda* bevanda = creaBevanda(1, "Bevanda", 1.0, frullato, "Bar");
+    Bevanda* bevanda = creaBevanda(1, "Gin Tonic", 1.0, cocktail, "barLSO");
     bevandaToJson(bevanda);
     printf("\n");
 
-    Ingrediente* ingrediente = creaIngrediente("Ingrediente");
+    Ingrediente* ingrediente = creaIngrediente("Gin");
     ingredienteToJson(ingrediente);
     printf("\n");
 
-    Utente* utente = creaUtente("email", "password", "nome", "cognome", "Bar");
+    Utente* utente = creaUtente("gianm@cacca.it", "caccamiao", "Gian Marco", "Addati", "barLSO");
     utenteToJson(utente);
     printf("\n");
-
     // Fine prova creazione oggetti e serializzazione in JSON
 
-
-
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
 
     // Inizio prova deserializzazione in oggetti e stampa a video
-    char* jsonBar = "{\"nome\":\"Bar\"}";
+    char* jsonBar = "{\"nome\":\"barLSO\"}";
     Bar* bar2 = jsonToBar(jsonBar);
     printf("\n");
 
-    char* jsonBevanda = "{\"id\":2, \"nome\":\"cccevanda\", \"prezzo\":5.99, \"tipo\":frullato, \"bar_nome\":\"car\"}";
+    char* jsonBevanda = "{\"id\":1, \"nome\":\"Gin Tonic\", \"prezzo\":7, \"tipo\":cocktail, \"bar_nome\":\"barLSO\"}";
     Bevanda* bevanda2 = jsonToBevanda(jsonBevanda);
     printf("\n");
 
-    char* jsonIngrediente = "{\"nome\":\"Ingrediente\"}";
+    char* jsonIngrediente = "{\"nome\":\"Gin\"}";
     Ingrediente* ingrediente2 = jsonToIngrediente(jsonIngrediente);
     printf("\n");
 
-    char* jsonUtente = "{\"email\":\"email\", \"password\":\"password\", \"nome\":\"nome\", \"cognome\":\"cognome\", \"bar_nome\":\"Bar\"}";
+    char* jsonUtente = "{\"email\":\"gianm@cacca.it\", \"password\":\"caccamiao\", \"nome\":\"Gian Marco\", \"cognome\":\"Addati\", \"bar_nome\":\"barLSO\"}";
     Utente* utente2 = jsonToUtente(jsonUtente);
     printf("\n");
-
     // Fine prova deserializzazione in oggetti e stampa a video
 
 
