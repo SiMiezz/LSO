31cdce3177b181c27ce13b83a3582ec8bbbf9a34
gianmadd <gi.addati@studenti.unina.it>
2023-03-26 12:12:32 +0200

    inizio prove connessione socket

--- Codice/Client/app/src/main/java/com/lso/client/Controller/UtenteController.java
+++ Codice/Client/app/src/main/java/com/lso/client/Controller/UtenteController.java
@@ -21,23 +21,36 @@ public class UtenteController {
     }
 
     public Utente getUtenteByEmailAndPassword(String email, String password){
+
+        System.out.println("inizio metodo");
+
         Utente utente = null;
         String result = null;
 
         connessioneController.startConnection();
+
+        System.out.println("dopo connessione");
 
         //richiesta
         connessioneController.writeOnOutput("getUtenteByEmailAndPassword");
         connessioneController.writeOnOutput(email);
         connessioneController.writeOnOutput(password);
 
+        System.out.println("dopo scrittura");
+
         //ricezione
         result = connessioneController.readFromInput();
+
+        System.out.println("dopo lettura");
 
         // conversione da json ad oggetto
         utente = jsonToUtente(result);
 
+        System.out.println("dopo conversione");
+
         connessioneController.closeConnection();
+
+        System.out.println("dopo chiusura");
 
         return utente;
     }
